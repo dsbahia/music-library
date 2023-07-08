8b74073e0a8d52b3959237bb96634f87a11391d1
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-07-08 10:54:40 +0100

    Used db.query() to insert a new artist into artist table and return 201

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,6 +1,12 @@
-const createArtist = (req, res) => {
+const db = require('../db/index.js');
+
+const createArtist = async (req, res) => {
     try {
-        res.status(201).json(req.body);
+        const { name, genre } = req.body;
+        const insertQuery = `INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *`;
+        const values = [name, genre];
+        const { rows: [newArtist] } = await db.query(insertQuery, values);
+        res.status(201).json(newArtist);
     } catch (error) {
         res.status(500).send('An error occurred while creating the artist.');
     }
