7ba553e900bc0a36eec9f35a3a27901010c1f89a
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-07-09 20:39:19 +0100

    Created updateArtist controller

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,15 +1,69 @@
 const db = require('../db/index.js');
 
 const createArtist = async (req, res) => {
-    try {
-        const { name, genre } = req.body;
-        const insertQuery = `INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *`;
-        const values = [name, genre];
-        const { rows: [newArtist] } = await db.query(insertQuery, values);
-        res.status(201).json(newArtist);
-    } catch (error) {
-        res.status(500).send('An error occurred while creating the artist.');
-    }
+  try {
+    const { name, genre } = req.body;
+    const insertQuery = `INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *`;
+    const values = [name, genre];
+    const {
+      rows: [newArtist],
+    } = await db.query(insertQuery, values);
+    res.status(201).json(newArtist);
+  } catch (error) {
+    res.status(500).json('An error occurred while creating the artist.');
+  }
 };
 
-module.exports = { createArtist };
+const readAllArtists = async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM Artists');
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json('An error occurred while reading artists.');
+  }
+};
+
+const readArtistById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const query = 'SELECT * FROM Artists WHERE id = $1';
+    const { rows } = await db.query(query, [id]);
+
+    if (!rows[0]) {
+      res.status(404).json({ message: `artist ${id} does not exist` });
+    } else {
+      res.status(200).json(rows[0]);
+    }
+  } catch (error) {
+    res.status(500).json('An error occurred while fetching the artist.');
+  }
+};
+
+const updateArtist = async (req, res) => {
+  const { id } = req.params;
+  const { name, genre } = req.body;
+  let query, params;
+
+  if (name && genre) {
+    query = 'UPDATE Artists SET name = $1, genre = $2 WHERE id = $3 RETURNING *';
+    params = [name, genre, id];
+  } else if (name) {
+    query = 'UPDATE Artists SET name = $1 WHERE id = $2 RETURNING *';
+    params = [name, id];
+  } else if (genre) {
+    query = 'UPDATE Artists SET name = $1 WHERE id = $2 RETURNING *';
+    params = [genre, id];
+  }
+
+  try {
+    const { rows: [artist] } = await db.query(query, params);
+    if (!artist) {
+        return res.status(404).json({ message: `artist ${id} does not exist` });
+    } 
+    res.status(200).json(artist);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while updating the artist.' });
+  }
+};
+
+module.exports = { createArtist, readAllArtists, readArtistById, updateArtist };
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { createArtist } = require('../controllers/artist');
+const { createArtist, readAllArtists, readArtistById, updateArtist } = require('../controllers/artist');
 
 const artistRouter = express.Router();
 
 artistRouter.post('/', createArtist);
+artistRouter.get('/', readAllArtists);
+artistRouter.get('/:id', readArtistById);
+artistRouter.patch('/:id', updateArtist);
 
 module.exports = artistRouter;
